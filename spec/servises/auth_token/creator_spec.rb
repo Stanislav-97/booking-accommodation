6e599dcad16bc00c6b473775fa433b56e1891fc6
Stanislav <s.zaporozhan97@mail.ru>
2024-08-01 21:09:16 +0600

    Изменения в rspec auth_token

--- spec/servises/auth_token/creator_spec.rb
+++ spec/servises/auth_token/creator_spec.rb
@@ -3,21 +3,29 @@ require "rails_helper"
 RSpec.describe AuthTokens::Creator, type: :service do
   subject(:service_call) { described_class.new(user).call }
 
-  let(:user) { create(:user) }
+  context "create a token" do 
+    let(:user) { create(:user) }
 
-  let(:auth_token) { create(:auth_token, user:) }
+    let(:auth_token) { create(:auth_token, user:) }
 
-  it "token created" do
-    expect { service_call }.to change(AuthToken, :count).by(1)
+    it "token created" do
+      expect { service_call }.to change(AuthToken, :count).by(1)
+    end
+
+    it "token current user" do
+      service_call
+      expect(AuthToken.last).to have_attributes(user_id: user.id)
+    end
   end
 
-  it "token current user" do
-    service_call
-    expect(AuthToken.last).to have_attributes(user_id: user.id)
-  end
+  context "token update" do 
+    let(:user) { create(:user) }
 
-  it "token update" do
-    service_call
-    expect(:auth_token).not_to eq(user.auth_token.token)
+    let!(:auth_token) { create(:auth_token, user:) }
+
+    it "token update" do
+      service_call
+      expect(AuthToken.last).not_to eq(auth_token.token)
+    end
   end
 end
